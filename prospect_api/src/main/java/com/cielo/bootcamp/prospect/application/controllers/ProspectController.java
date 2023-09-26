af0c9a8581089da33fe093c37c5cf060f3c4d326
mateus simão <mateus.david@outlook.com>
2023-09-26 20:15:04 -0300

    feat(ProspectService, ProspectCotroller) - adicionando metodos para realizar o crud e os aplicando nas rotas

--- prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/controllers/ProspectController.java
+++ prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/controllers/ProspectController.java
@@ -1,34 +1,61 @@
 package com.cielo.bootcamp.prospect.application.controllers;
 
 import com.cielo.bootcamp.prospect.application.dtos.ProspectDTO;
+import com.cielo.bootcamp.prospect.application.services.ProspectService;
+import com.cielo.bootcamp.prospect.domain.Prospect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/prospects")
 public class ProspectController {
 
+    @Autowired
+    private ProspectService service;
+
     @GetMapping
-    public String getAll() {
-        return "Route getAll";
+    public ResponseEntity<List<Prospect>> getAll() {
+        List<Prospect> prospectList = this.service.findAll();
+
+        return new ResponseEntity<>(prospectList, HttpStatus.OK);
     }
 
     @GetMapping("/{prospectId}")
-    public String get(@PathVariable Long prospectId) {
-        return "Route get";
+    public ResponseEntity<Prospect> get(@PathVariable Long prospectId) {
+        try {
+            Prospect prospect = this.service.findProspectById(prospectId);
+            return new ResponseEntity<>(prospect, HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
-    public String create(@RequestBody ProspectDTO prospectDTO) {
-        return "Route create";
+    public ResponseEntity<Prospect> create(@RequestBody ProspectDTO prospectDTO) {
+        Prospect prospect = this.service.save(prospectDTO);
+
+        return new ResponseEntity<>(prospect, HttpStatus.CREATED);
     }
 
     @PutMapping("/{prospectId}")
-    public String update(@PathVariable Long prospectId) {
-        return "Route update";
+    public ResponseEntity<Prospect> update(@PathVariable Long prospectId, @RequestBody ProspectDTO prospectDTO) throws Exception {
+        Prospect prospect = this.service.update(prospectId, prospectDTO);
+
+        return new ResponseEntity<>(prospect, HttpStatus.OK);
     }
 
     @DeleteMapping("/{prospectId}")
-    public String delete(@PathVariable Long prospectId) {
-        return "Route delete";
+    public ResponseEntity<Void> delete(@PathVariable Long prospectId) {
+        try {
+            this.service.delete(prospectId);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/services/ProspectService.java
+++ prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/services/ProspectService.java
@@ -1,7 +1,53 @@
 package com.cielo.bootcamp.prospect.application.services;
 
+import com.cielo.bootcamp.prospect.application.dtos.ProspectDTO;
+import com.cielo.bootcamp.prospect.application.repositories.ProspectRepository;
+import com.cielo.bootcamp.prospect.domain.Prospect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProspectService {
+
+    @Autowired
+    private ProspectRepository repository;
+
+    public Prospect findProspectById(Long id) throws Exception {
+        return this
+                .repository
+                .findProspectById(id)
+                .orElseThrow(() -> new Exception("Prospect not found"));
+    }
+
+    public List<Prospect> findAll() {
+        return this.repository.findAll();
+    }
+
+    public Prospect save(ProspectDTO prospectDTO) {
+        return  this.repository.save(new Prospect(prospectDTO));
+    }
+
+    public Prospect update(Long id, ProspectDTO prospectDTO) throws Exception{
+        Prospect prospect = this.findProspectById(id);
+
+        prospect.setName(prospectDTO.name());
+        prospect.setMCC(prospectDTO.MCC());
+        prospect.setDocument(prospectDTO.document());
+        prospect.setContactDocument(prospectDTO.contactDocument());
+        prospect.setContactName(prospectDTO.contactName());
+        prospect.setContactEmail(prospectDTO.contactEmail());
+        prospect.setClientType(prospectDTO.clientType());
+
+        this.repository.save(prospect);
+
+        return prospect;
+    }
+
+    public void delete(Long id) throws Exception {
+        Prospect prospect = this.findProspectById(id);
+
+        this.repository.delete(prospect);
+    }
 }
