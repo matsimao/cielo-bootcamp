83fba58ab127d1566bf52b1a8552d7f5fed55368
mateus simão <mateus.david@outlook.com>
2023-09-27 00:38:14 -0300

    feat(ProspectService) - executando validações e criando validação pesonalizada para o documento de cliente do tipo business/juridico e individual/fisico

--- prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/services/ProspectService.java
+++ prospect_api/src/main/java/com/cielo/bootcamp/prospect/application/services/ProspectService.java
@@ -2,9 +2,13 @@ package com.cielo.bootcamp.prospect.application.services;
 
 import com.cielo.bootcamp.prospect.application.dtos.ProspectDTO;
 import com.cielo.bootcamp.prospect.application.repositories.ProspectRepository;
+import com.cielo.bootcamp.prospect.domain.ClientType;
 import com.cielo.bootcamp.prospect.domain.Prospect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.*;
 
 import java.util.List;
 
@@ -13,6 +17,34 @@ public class ProspectService {
 
     @Autowired
     private ProspectRepository repository;
+
+    @Autowired
+    private Validator validator;
+
+    private final Logger LOG = LoggerFactory.getLogger(ProspectService.class);
+
+    public void validateProspect(Prospect prospect) throws  Exception{
+        Errors errors = new BeanPropertyBindingResult(prospect, "prospect");
+
+        validator.validate(prospect, errors);
+
+        if (prospect.getClientType() == ClientType.INDIVIDUAL_CUSTOMER) {
+            if (prospect.getDocument().length() > 11) {
+                errors.rejectValue("document", "length.exceeded", "O campo 'document' deve ter entre 1 e 11 caracteres");
+            }
+        } else {
+            if (prospect.getDocument().length() > 13) {
+                errors.rejectValue("document", "length.exceeded", "O campo 'document' deve ter entre 1 e 13 caracteres");
+            }
+        }
+
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                LOG.error(error.getDefaultMessage());
+            }
+            throw new Exception("A validação falhou.");
+        }
+    }
 
     public Prospect findProspectById(Long id) throws Exception {
         return this
@@ -25,7 +57,10 @@ public class ProspectService {
         return this.repository.findAll();
     }
 
-    public Prospect save(ProspectDTO prospectDTO) {
+    public Prospect save(ProspectDTO prospectDTO) throws Exception{
+        Prospect prospect = new Prospect(prospectDTO);
+
+        this.validateProspect(prospect);
         return  this.repository.save(new Prospect(prospectDTO));
     }
 
@@ -40,9 +75,8 @@ public class ProspectService {
         prospect.setContactEmail(prospectDTO.contactEmail());
         prospect.setClientType(prospectDTO.clientType());
 
-        this.repository.save(prospect);
-
-        return prospect;
+        this.validateProspect(prospect);
+        return this.repository.save(prospect);
     }
 
     public void delete(Long id) throws Exception {
